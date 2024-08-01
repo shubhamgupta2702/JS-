5e5522d075f370e1b67e8e37bdd43e33a677c72e
Shubham Gupta <147174815+shubhamgupta2702@users.noreply.github.com>
2024-08-01 06:03:23 +0000

    datatypes done

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,29 @@
+"use strict";  // treat all Js Code as newer version
+
+//alert(3+3)  // we are using nodejs, not browser
+
+console.log(3 +
+    3);   //code readability should be high..
+
+    console.log("Shubham")
+
+
+    let name = "Shubham"
+    let age = 18
+    let isLogggedIn = false
+     let state = null
+
+
+    //number ==> 2 to the power 53 --> range
+    //bigint
+    //string ==> ""
+    //boolean ==> true/ false
+    //null ==> standalone value
+    //undefined ==> value not assignes yet!!
+    //symbol==> unique
+
+    //object
+
+    console.log(typeof isLogggedIn);
+    console.log(typeof null);
+    console.log(typeof undefined);
