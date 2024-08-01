33ae795d1c1a2d977ee0fe004c6ec05ec115ab04
Shubham Gupta <147174815+shubhamgupta2702@users.noreply.github.com>
2024-08-01 06:51:22 +0000

    Type Conversion Done

---
+++ 01_basics/03_conversionOperations.js
@@ -0,0 +1,45 @@
+let score = "33abc"
+
+console.log(typeof score);
+console.log(typeof(score));
+
+
+let valueInNumber = Number(score)
+console.log(typeof valueInNumber);
+console.log(valueInNumber);
+
+let temp = null
+ console.log(typeof temp);
+ console.log(typeof (temp));
+
+ let tempInChange = Number(temp)
+ console.log(typeof tempInChange);
+console.log(tempInChange);
+
+
+let ud = undefined
+
+console.log(typeof ud);
+
+let changeUd = Number(ud)
+console.log(typeof changeUd);
+console.log(changeUd);
+
+// "33" ==> 33
+// "33abc' ==> NaN
+// true ==>1; false ==>0
+
+let isLogggedin = "shubham"
+
+let booleanIsLoggedIn = Boolean(isLogggedin)
+console.log(booleanIsLoggedIn);
+
+// 0 ==> false; 1 ==> true
+// "" ==> FALSE
+// "shubham" ==> true
+
+let someNum = 33
+let stringNumber = String(someNum)
+console.log(someNum);
+console.log(typeof someNum);
+console.log(typeof stringNumber);
