af12840dedd4f4dad26755594c74860edb5933e1
Shubham Gupta <147174815+shubhamgupta2702@users.noreply.github.com>
2024-08-02 05:53:49 +0000

    Comparisons Done

---
+++ 01_basics/04_comparison.js
@@ -0,0 +1,34 @@
+// console.log(2 >1);
+// console.log( 2 >= 1);
+// console.log( 2 <1);
+// console.log( 2 == 1);
+// console.log(2 != 1);
+  
+
+//Not a Good practice!!
+console.log("2" >1);
+console.log("02" > 1);
+
+// Very Unprofessional conversions
+
+console.log(null > 0);
+console.log(null == 0);
+console.log(null >= 0);
+
+console.log(undefined = 0);
+console.log(undefined >0);
+console.log(undefined < 0);
+
+// === --> Strict check. It also check datatypes !!!
+
+console.log("3"  === 3);
+
+
+
+
+
+
+
+
+
+
