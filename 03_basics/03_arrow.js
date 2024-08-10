3df053b3f34e042b14f454dd0f86990f9a3bbcc3
Shubham Gupta <147174815+shubhamgupta2702@users.noreply.github.com>
2024-08-10 19:08:20 +0000

    this and arrow function

---
+++ 03_basics/03_arrow.js
@@ -0,0 +1,56 @@
+const user = {
+    username: "panda",
+    price: "99",
+
+    welcomeMsg: function(){
+        console.log(`${this.username} , welcome to website`);
+        console.log(this);
+        
+        
+    }
+}
+user.welcomeMsg()
+// user.username = "shubham"
+user.welcomeMsg()
+
+console.log(this);
+
+// function panda() {
+//     let username = "shubh"
+//     console.log(this.username);
+    
+    
+// }
+
+// const panda = function(){
+//     let username = "shubh"
+//     console.log(this.username);
+// }
+
+// panda() 
+
+// ---------------------------------------  ARROW FUNCTIONS --------------------------------------
+
+
+const panda = () => {
+    let username = "shubh"
+    console.log(this);
+}
+// panda()
+
+// 1ST WAY OF DECLARING ARROW FUNCTIONS
+
+
+// const addTwo = (num1 , num2) => {
+//     return num1 + num2
+// }
+
+// 2ND WAY OF DECLARING ARROW FUNCTIONS
+
+// const addTwo = (num1 , num2) => num1 + num2  // 
+
+// WRD WAY OF DECLARING ARROW FUNCTIONS   #MOST USED METHOD
+
+const addTwo = (num1 , num2) => (num1 + num2)  // no need to write return while using parethesis
+
+console.log(addTwo(7 , 8));
