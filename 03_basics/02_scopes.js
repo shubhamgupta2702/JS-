97a1584cde2082e62702dd4e1589f9fac216644b
Shubham Gupta <147174815+shubhamgupta2702@users.noreply.github.com>
2024-08-10 16:46:45 +0000

    scopes ongoing

--- 03_basics/02_scopes.js
+++ 03_basics/02_scopes.js
@@ -1,12 +1,35 @@
-
+//    {} --> Scope
+var c = 300 
+let a = 400    // Let is Block Scoped.
 if (true) {
 
     let a = 10
     const b = 20
     var c = 30
+    // console.log("Inner:" , a);
+    
+}
+//  console.log(a);
+ 
+// console.log(a);
+// console.log(b);
+// console.log(c);
+
+
+function one(params) {
+    const username= "Shubham"
+
+    function two(params) {
+        const website = "Telegram"
+        console.log(username);
+        
+        
+    }
+    //console.log(website);  --> scope ends here because its scope is in function two
+
+
+    two()
 }
 
-console.log(a);
-console.log(b);
-console.log(c);
+one()
 
