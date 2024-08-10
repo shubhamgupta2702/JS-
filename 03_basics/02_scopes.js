fc385b5c4af1a55a8806013f0bd54a7453e93de9
Shubham Gupta <147174815+shubhamgupta2702@users.noreply.github.com>
2024-08-10 18:28:36 +0000

    arrow begins

--- 03_basics/02_scopes.js
+++ 03_basics/02_scopes.js
@@ -25,11 +25,45 @@ function one(params) {
         
         
     }
-    //console.log(website);  --> scope ends here because its scope is in function two
+    //console.log(website);  --> scope ends here, because its scope is in function two.
 
 
-    two()
+    // two()
 }
 
-one()
+// one()
 
+if (true) {
+    const username = "shubham"
+    if (username === "shubham") {
+        const website = " youtube"
+        console.log(username + website);
+        
+        
+    }
+    // console.log(website);    error --> scope not defined
+    
+    
+}
+// console.log(username);  error --> scope is not defined here for username
+
+
+
+// ========================   intresting   ================================
+//              2 WAYS OF DECLARING A FUNCTION
+
+addone(5) // --> we can access this function | this way of declaration supports early access.
+console.log(addone(5));
+
+function addone(num) {
+    return num +1
+    
+}
+
+
+// addTwo(5)   --> we cannot access this function first| this way of declaration not support early access.
+const addTwo = function(num) {
+    return num +2
+    
+}
+addTwo(5)
