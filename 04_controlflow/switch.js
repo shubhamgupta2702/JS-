49d1f55500a18a1776a360574b1a033a95894895
Shubham Gupta <147174815+shubhamgupta2702@users.noreply.github.com>
2024-08-12 08:00:51 +0000

    control flow

---
+++ 04_controlflow/switch.js
@@ -0,0 +1,52 @@
+// switch (key) {
+//     case value:
+        
+//         break;
+
+//     default:
+//         break;
+// }
+
+// const month = 3
+
+// switch (month) {
+//     case 1:
+//         console.log("jan");
+//         break;
+//     case 2:
+//         console.log("feb");
+//         break;
+//     case 3:
+//         console.log("march");
+//         break;
+//     case 4:
+//         console.log("april");
+//         break;
+
+//     default:
+//         console.log("Default case matched");
+        
+//         break;
+// }
+
+const month = "march"   // also used string to compare..
+
+switch (month) {
+    case "jan":
+        console.log("jan");
+        break;
+    case "feb":
+        console.log("feb");
+        break;
+    case "march":
+        console.log("march");
+        break;
+    case "april":
+        console.log("april");
+        break;
+
+    default:
+        console.log("Default case matched");
+        
+        break;
+}
