84cc941e59cefac4fbc41a12c3d9db9c64fdc1cb
Shubham Gupta <147174815+shubhamgupta2702@users.noreply.github.com>
2024-08-13 07:01:40 +0000

    iterations files

---
+++ 05_iterations/one.js
@@ -0,0 +1,17 @@
+// for
+
+// for (let index = 0; index < 10; index++) {
+//     const element = index
+//     if (element == 5) {
+//         console.log("5 is best number");
+//     }
+//     console.log(element);
+// }
+
+
+for (let let = 0; let < array.length; let++) {
+    console.log(`Outer Loop value: ${i}`);
+        for
+    
+    
+}
