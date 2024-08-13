a6183c96303dcc11714fcdac1559d1d85941d4db
Shubham Gupta <147174815+shubhamgupta2702@users.noreply.github.com>
2024-08-13 07:41:10 +0000

    for loop

--- 05_iterations/one.js
+++ 05_iterations/one.js
@@ -9,9 +9,37 @@
 // }
 
 
-for (let let = 0; let < array.length; let++) {
-    console.log(`Outer Loop value: ${i}`);
-        for
-    
-    
+for (let i = 1; i <= 10; i++) {
+    // console.log(`Outer Loop value: ${i}`);
+    for (let j = 1; j <= 10; j++) {
+        // console.log(` The inner loop value: ${j} and the outer loop value: ${i}`);
+        // console.log(i + "*" + j + "=" + i*j);
+    }
+}
+let myArr = ["flash", "batman", "superman"]
+console.log(myArr.length);
+    for (let index = 0; index < myArr.length; index++) {
+         const element = myArr[index];
+            // console.log(element);
+}
+
+// console.log(element);   No scope outside for loop
+
+//    BREAK AND CONTINUE
+
+for (let index = 0; index < 20; index++) {
+    if (index == 5) {
+        // console.log(`Detected 5`);
+        break    // when 5 detected it will wxit the loop
+    }
+    // console.log(`Value of i is ${index}`);
+}
+
+for (let index = 0; index < 10; index++) {
+    const element = index;
+    if (index == 5) {
+        console.log(` Detected 5`);
+        continue     // when  5 detected it will continue after 5 from 6
+    }
+    console.log(`Value of i is ${index}`);
 }
