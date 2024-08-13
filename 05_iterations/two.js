61a2f41af37a382f6be329efcd5bd0d3d26e1bf0
Shubham Gupta <147174815+shubhamgupta2702@users.noreply.github.com>
2024-08-13 07:56:18 +0000

    do whilw loops

---
+++ 05_iterations/two.js
@@ -0,0 +1,23 @@
+let index = 0
+
+while (index <= 10) {
+//    console.log(`Value of index is : ${index}`);
+   index++  // or index = index +1
+      
+}
+let myArray = ["flash" , "batman" , "Superman"]
+
+let arr =0
+while (arr < myArray.length) {
+    // console.log(`Value is ${myArray[arr]}`);
+    arr= arr +1
+    
+}
+
+let score = 14
+
+do{      // do while loop runs at least one time.
+    console.log(`Score is ${score}`);
+    score ++
+
+} while (score <= 13);
